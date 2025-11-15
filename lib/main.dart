514242c4143d1cb3396735a55292099f60470417
tour-dev <allanotieno95@gmail.com>
2025-11-15 17:02:04 +0300

    Updated demo counter message.

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: .center,
           children: [
-            const Text('You have pushed the button this many times:'),
+            const Text('You have pushed the add button this many times:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
