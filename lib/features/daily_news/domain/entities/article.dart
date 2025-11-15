96e6421af15fdf09c578a3b6dd20a9bd0a34b098
tour-dev <allanotieno95@gmail.com>
2025-11-15 18:11:41 +0300

    Added article entity to handle data manipulation.

---
+++ lib/features/daily_news/domain/entities/article.dart
@@ -0,0 +1,40 @@
+import 'package:equatable/equatable.dart';
+
+class ArticleEntity extends Equatable {
+  final int? id;
+  final String? author;
+  final String? title;
+  final String? description;
+  final String? url;
+  final String? urlToImage;
+  final String? publishedAt;
+  final String? content;
+  final String? source;
+
+  const ArticleEntity({
+    this.id,
+    this.author,
+    this.title,
+    this.description,
+    this.url,
+    this.urlToImage,
+    this.publishedAt,
+    this.content,
+    this.source,
+  });
+
+  @override
+  List<Object?> get props {
+    return [
+      id,
+      author,
+      title,
+      description,
+      url,
+      urlToImage,
+      publishedAt,
+      content,
+      source,
+    ];
+  }
+}
