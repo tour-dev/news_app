22917118f19574363f01dac1fd5785519325c525
tour-dev <allanotieno95@gmail.com>
2025-11-15 22:23:47 +0300

    Implemented fetching of news articles to be displayed on UI widget.

--- lib/features/daily_news/presentation/pages/home/daily_news.dart
+++ lib/features/daily_news/presentation/pages/home/daily_news.dart
@@ -1,21 +1,42 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
+import 'package:news_app/features/daily_news/presentation/bloc/article/remote/remote_article_state.dart';
 
 class DailyNews extends StatelessWidget {
   const DailyNews({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: _mainAppBar());
+    return Scaffold(appBar: _mainAppBar(), body: _mainBody());
   }
 
   AppBar _mainAppBar() {
     return AppBar(
-      title: const Text(
-        'Daily News',
-        style: TextStyle(
-          color: Colors.black
-        ),
-      ),
+      title: const Text('Daily News', style: TextStyle(color: Colors.black)),
+    );
+  }
+
+  BlocBuilder<RemoteArticlesBloc, RemoteArticlesState> _mainBody() {
+    return BlocBuilder<RemoteArticlesBloc, RemoteArticlesState>(
+      builder: (_, state) {
+        if (state is RemoteArticlesLoading) {
+          return const Center(child: CupertinoActivityIndicator());
+        }
+        if (state is RemoteArticlesError) {
+          return const Center(child: Icon(Icons.refresh));
+        }
+        if (state is RemoteArticlesDone) {
+          return ListView.builder(
+            itemBuilder: (context, index) {
+              return ListTile(title: Text('$index'));
+            },
+            itemCount: state.articles!.length,
+          );
+        }
+        return const SizedBox();
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news_app/config/theme/app_themes.dart';
+import 'package:news_app/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
+import 'package:news_app/features/daily_news/presentation/bloc/article/remote/remote_article_event.dart';
 import 'package:news_app/features/daily_news/presentation/pages/home/daily_news.dart';
 import 'package:news_app/injection_container.dart';
 
@@ -15,11 +18,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'News App',
-      theme: theme(),
-      debugShowCheckedModeBanner: false,
-      home: DailyNews(),
+    return BlocProvider<RemoteArticlesBloc>(
+      create: (BuildContext context) => sl()..add(GetArticles()),
+      child: MaterialApp(
+        title: 'News App',
+        theme: theme(),
+        debugShowCheckedModeBanner: false,
+        home: DailyNews(),
+      ),
     );
   }
 }
